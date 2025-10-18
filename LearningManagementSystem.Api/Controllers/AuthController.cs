e7639fe294b1971e7af7e425b7f9722f61ed48d2
bwx1y <nedynugroho2007@gmail.com>
2025-10-18 18:45:53 +0700

    add login

---
+++ LearningManagementSystem.Api/Controllers/AuthController.cs
@@ -0,0 +1,26 @@
+using LearningManagementSystem.Api.Extention;
+using LearningManagementSystem.Application.DTO.Auth;
+using LearningManagementSystem.Application.Service;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LearningManagementSystem.Api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AuthController(AuthService authService) : ControllerBase
+{
+    [HttpPost("login")]
+    public async Task<IActionResult> Login(LoginRequest request)
+    {
+        var token = await authService.Login(request.Email, request.Password);
+        if (token == null)
+            return this.ValidationError(
+                statusCode: StatusCodes.Status401Unauthorized,
+                key: "Login",
+                message: "Email or Password wrong",
+                title: "login failed",
+                detail: "invalid credentials"
+            );
+        return Ok(token);
+    }
+}
